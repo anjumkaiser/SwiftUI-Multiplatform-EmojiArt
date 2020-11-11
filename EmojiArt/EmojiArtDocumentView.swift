373af8aeadfb7176005c7e2a34913ae89223505d
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2020-11-11 15:21:43 +0500

    Drag and drop image from other programs on iPadOS

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -12,14 +12,36 @@ struct EmojiArtDocumentView: View {
     @ObservedObject var document: EmojiArtDocument
     
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(EmojiArtDocument.palette.map { String($0) }, id: \.self) { emoji in
-                    Text(emoji)
-                        .font (Font.system(size: defaultEmojiSize))
+        VStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(EmojiArtDocument.palette.map { String($0) }, id: \.self) { emoji in
+                        Text(emoji)
+                            .font (Font.system(size: defaultEmojiSize))
+                    }
                 }
             }
+            .padding(.horizontal)
+            Color.white.overlay(
+                Group {
+                    if self.document.backgroundImage != nil {
+                        Image(uiImage: self.document.backgroundImage!)
+                    }
+                }
+            )
+                .edgesIgnoringSafeArea([.horizontal, .bottom])
+                .onDrop(of: ["public.image"], isTargeted: nil ) { providers, location in
+                    return self.drop(providers: providers)
+                }
         }
+    }
+    
+    private func drop(providers: [NSItemProvider]) -> Bool {
+        let found = providers.loadFirstObject(ofType: URL.self) { url in
+            print("dropper \(url)")
+            self.document.setBackgroundURL(url)
+        }
+        return found
     }
     
     private let defaultEmojiSize: CGFloat = 40
